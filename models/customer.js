cfd441af32e1f2a372a127f533e612be1ca116dc
anthony <anthony.pate@gobonzi.com>
2019-04-08 16:13:48 -0700

    vidly version 1

--- models/customer.js
+++ models/customer.js
@@ -1,34 +1,53 @@
-const Joi = require('joi');
-const mongoose = require('mongoose');
+const Joi = require("joi");
+const mongoose = require("mongoose");
 
-const Customer = mongoose.model('Customer', new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50
-  },
-  isGold: {
-    type: Boolean,
-    default: false
-  },
-  phone: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50
-  }
-}));
+const Customer = mongoose.model(
+  "Customer",
+  new mongoose.Schema({
+    firstName: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 50
+    },
+    lastName: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 50
+    },
+    isGold: {
+      type: Boolean,
+      default: false
+    },
+    phone: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50
+    }
+  })
+);
 
 function validateCustomer(customer) {
   const schema = {
-    name: Joi.string().min(5).max(50).required(),
-    phone: Joi.string().min(5).max(50).required(),
+    firstName: Joi.string()
+      .min(2)
+      .max(50)
+      .required(),
+    lastName: Joi.string()
+      .min(2)
+      .max(50)
+      .required(),
+    phone: Joi.string()
+      .min(5)
+      .max(50)
+      .required(),
     isGold: Joi.boolean()
   };
 
   return Joi.validate(customer, schema);
 }
 
-exports.Customer = Customer; 
+exports.Customer = Customer;
 exports.validate = validateCustomer;

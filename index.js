31c3762e5f32a6f40b70e2b49a5bc8168bde0910
anthony <anthony.pate@gobonzi.com>
2019-04-10 09:21:42 -0700

    test

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ require("./startup/validation")();
 require("./startup/prod")(app);
 
 const port = process.env.PORT || config.get("port");
-const server = app.listen(port, () =>
+const server = app.listen(port, "0.0.0.0", () =>
   winston.info(`Listening on port ${port}...`)
 );
 
